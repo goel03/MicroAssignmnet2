648a17602adb0ef88bc1f1536589c50c2b458745
Prachi Goel <prachigoel1103@gmail.com>
2022-12-23 17:51:36 +0530

    added fetch category api

--- my-app/src/api/discover.js
+++ my-app/src/api/discover.js
@@ -8,3 +8,23 @@ export async function getCategories() {
     }
 }
 
+export async function setFavoriteImage(imageId) {
+    const reqUrl = `http://localhost:3000/api/like/${imageId}`;
+    const result = await axios.get(reqUrl);
+    if (result.data) {
+        return result.data;
+    }
+}
+
+export async function getGallery(
+    categoryName,
+    sortByLike,
+    sortByDate,
+    shuffle
+) {
+    const reqUrl = `http://localhost:3000/api/discover/${categoryName}?sortByLike=${sortByLike}&sortByDate=${sortByDate}&shuffle=${shuffle}`;
+    const result = await axios.get(reqUrl);
+    if (result.data) {
+        return result.data;
+    }
+}
